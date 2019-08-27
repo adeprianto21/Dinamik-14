55f9bc12901ba612d4fe593831ef305bfd7ca3fd
adeprianto21 <adeprianto21@upi.edu>
2019-08-27 17:51:28 +0700

    Initial Commit

---
+++ resources/js/main.js
@@ -0,0 +1,111 @@
+$(document).ready(function () {
+
+    let headerHomePosition = $("#abstraction-section").position().top;
+
+    if ($(window).width() < 900) {
+        $("#nav-menu-container").addClass("overlay-navbar");
+        $("#nav-item-list").addClass("overlay-navbar-item").removeClass("nav-dinamik-item");
+    }
+
+    $(window).resize(function () {
+        if ($(window).width() < 900) {
+            $("#nav-menu-container").addClass("overlay-navbar");
+            $("#nav-item-list").addClass("overlay-navbar-item").removeClass("nav-dinamik-item");
+        } else {
+            $("#nav-menu-container").removeClass("overlay-navbar");
+            $("#nav-item-list").removeClass("overlay-navbar-item").addClass("nav-dinamik-item");
+        }
+    });
+
+    $(".navbar-toggle").click(function () {
+        $("#nav-menu-container").css("display", "flex");
+        $(".overlay-navbar").css("height", "100%");
+        $(".close-btn-group").toggleClass("change");
+        $("body").css("overflow", "hidden");
+    });
+
+    $(".close-btn-group").click(function () {
+        $(".overlay-navbar").css("height", "0%");
+        $(".close-btn-group").toggleClass("change");
+        $("body").css("overflow", "auto");
+    });
+
+    $(window).scroll(function () {
+
+        var scrollTop = $(window).scrollTop();
+
+        $('.layer').each(function () {
+            var layer = $(this);
+            var dataSpeed = layer.data('speed');
+            var offsetY = -(scrollTop * dataSpeed);
+            layer.css('transform', 'translateY(' + offsetY + 'px)');
+        });
+
+        if ($("header").position().top > ($("#abstraction-section").position().top - 100)) {
+            $("header").css({ "background-color": "rgba(0,0,0,0.9)", "transition": "all ease 0.5s" });
+        } else {
+            $("header").css({ "background-color": "rgba(0,0,0,0)", "transition": "all ease 0.5s" });
+        }
+
+    });
+
+    $(".form-input").each(function () {
+        $(this).focus(function () {
+            $(this).css("border-bottom", "2px solid #ffee00");
+            $(this).prev().addClass("form-label-focus");
+            $(this).next().css({ "color": "#ffee00", "transition": "all 0.6s ease" });
+        });
+
+        $(this).focusout(function () {
+            if ($(this).val() == 0) {
+                $(this).css("border-bottom", "2px solid #d600ff");
+                $(this).prev().removeClass("form-label-focus");
+                $(this).next().css({ "color": "#d600ff", "transition": "all 0.6s ease" });
+            }
+        });
+    });
+
+    $(".show-pass").on("click", function () {
+
+        $(this).children("i").toggleClass("fa-eye").toggleClass("fa-eye-slash");
+
+        if ($(this).prev().attr("type") == "password") {
+            $(this).prev().attr("type", "text");
+        } else {
+            $(this).prev().attr("type", "password");
+        }
+    })
+
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 10,
+        stagePadding: 50,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplaySpeed: 1000,
+        autoplayHoverPause: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                stagePadding: 0
+            },
+            680: {
+                items: 2
+            },
+            980: {
+                items: 3
+            }
+        }
+    });
+
+    $('#prev-competition-carousel').click(function () {
+        $(".owl-carousel").trigger('prev.owl.carousel');
+    })
+
+    $('#next-competition-carousel').click(function () {
+        $(".owl-carousel").trigger('next.owl.carousel');
+    })
+
+});
+
---
+++ resources/views/home.blade.php
@@ -0,0 +1,203 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#000" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"
+        integrity="sha256-UhQQ4fxEeABh4JrcmAJ1+16id/1dnlOEVCFOxDef9Lw=" crossorigin="anonymous" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.css"
+        integrity="sha256-nXBV7Gr2lU0t+AwKsnS05DYtzZ81oYTXS6kj7LBQHfM=" crossorigin="anonymous" />
+
+    <link rel="icon" href="resources/img/dinamik.png">
+    <link rel="stylesheet" href="resources/css/styles.css">
+
+    <title>Dinamik</title>
+</head>
+
+<body>
+
+    <section id="landing-page-hero">
+        <div class="parallax-go-up">
+            <div class="bg-dinamik">
+            </div>
+            <div class="bg-dinamik">
+            </div>
+        </div>
+
+        <div class="layer" data-speed="0">
+            <img class="gradient" src="resources/img/Parallax/layer1.png" alt="">
+        </div>
+        <div class="layer" data-speed="-0.70">
+            <div class="layer-2">
+                <img src="resources/img/Parallax/layer2.png" class="map" alt="">
+                <span class="diesnatalis">diesnatalis</span>
+                <span class="nov-2019">November 2019</span>
+                <span class="number">008980555555400000</span>
+            </div>
+        </div>
+        <div class="layer layer-3" data-speed="-0.63">
+            <img src="resources/img/Parallax/layer3.png" class="dinamik" alt="">
+        </div>
+        <div class="layer" data-speed="-0.20">
+            <div class="layer-4">
+                <img src="resources/img/Parallax/layer4.png" class="tv" alt="">
+                <img src="resources/img/rotate.png" class="rotate-scroll" alt="">
+            </div>
+        </div>
+        <div class="layer" data-speed="0">
+            <img src="resources/img/Parallax/layer5.png" class="gradient" alt="">
+        </div>
+    </section>
+
+    <header class="header-home">
+
+        <nav>
+            <div class="brand-dinamik">
+                <a href="{{url('/')}}">
+                    <img src="resources/img/Parallax/layer3.png" alt="">
+                </a>
+            </div>
+            <div class="navbar-toggle">
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+            </div>
+            <div id="nav-menu-container">
+                <div class="close-btn-group">
+                    <div class="bar1"></div>
+                    <div class="bar2"></div>
+                </div>
+                <div class="nav-dinamik-item" id="nav-item-list">
+                    <ul>
+                        <li><a href="{{url('/')}}">Home</a></li>
+                        <li><a href="#">Competition</a></li>
+                        <li><a href="#">Acara</a></li>
+                        <li><a href="#">Info</a></li>
+                        <li><a href="{{url('/daftar')}}">Register</a></li>
+                        <li><a href="#">Login</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </header>
+
+    <main>
+
+        <section class="home-sections" id="abstraction-section">
+
+            <div class="header-section">
+                <div class="header-stroke header-animation">
+                    <span>about</span>
+                    <span>about</span>
+                    <span>about</span>
+                    <span>about</span>
+                    <span>about</span>
+                </div>
+            </div>
+
+            <div class="dinamik-abstract">
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est ipsa recusandae id reiciendis placeat
+                    enim
+                    dolor optio, excepturi, veniam aliquid aliquam tempora reprehenderit illo deleniti ex! Saepe atque
+                    et
+                    doloribus!''
+                </p>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est ipsa recusandae id reiciendis placeat
+                    enim
+                    dolor optio, excepturi, veniam aliquid aliquam tempora reprehenderit illo deleniti ex! Saepe atque
+                    et
+                    doloribus!''
+                </p>
+                <p>
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est ipsa recusandae id reiciendis placeat
+                    enim
+                    dolor optio, excepturi, veniam aliquid aliquam tempora reprehenderit illo deleniti ex! Saepe atque
+                    et
+                    doloribus!''
+                </p>
+            </div>
+
+        </section>
+
+        <section class="home-sections" id="competition-section">
+
+            <div class="header-section">
+                <div class="header-stroke header-animation">
+                    <span>competition</span>
+                    <span>competition</span>
+                    <span>competition</span>
+                    <span>competition</span>
+                    <span>competition</span>
+                </div>
+            </div>
+
+            <div class="competition-slide owl-carousel">
+                <div class="competition-container">
+                    <img src="resources/img/Parallax/layer2.png" alt="">
+                    <span>Lomba 1</span>
+                </div>
+                <div class="competition-container">
+                    <img src="resources/img/Parallax/layer2.png" alt="">
+                    <span>Lomba 2</span>
+                </div>
+                <div class="competition-container">
+                    <img src="resources/img/Parallax/layer2.png" alt="">
+                    <span>Lomba 3</span>
+                </div>
+                <div class="competition-container">
+                    <img src="resources/img/Parallax/layer2.png" alt="">
+                    <span>Lomba 4</span>
+                </div>
+                <div class="competition-container">
+                    <img src="resources/img/Parallax/layer2.png" alt="">
+                    <span>Lomba 5</span>
+                </div>
+            </div>
+
+            <div class="competition-button-group">
+                <div class="competition-prev">
+                    <button id="prev-competition-carousel">
+                        <</button> </div> <div class="button-login">
+                            <a href="#"></a>
+                </div>
+                <div class="competition-next">
+                    <button id="next-competition-carousel">></button>
+                </div>
+            </div>
+
+        </section>
+
+    </main>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"
+        integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous">
+    </script>
+
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.js"
+        integrity="sha256-251s88HEsEfGL2RufZmRwGohKTHDYr9T+aJAazDwlGY=" crossorigin="anonymous"></script>
+
+    <script src="resources/js/main.js"></script>
+
+</body>
+
+</html>
